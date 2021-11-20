eb71711c899b4fc013bf8877b63f1194d4012c73
Cassio Diego <cassio_diego123@hotmail.com>
2021-11-20 16:04:07 -0300

    Commit Inicial

---
+++ WebApplication1/Models/EmpresaModel.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApplication1.Models
+{
+    public class EmpresaModel
+    {
+        private int codigo { get; set; }
+        private string nome { get; set; }
+        private string nomeFantasia { get; set; }
+        private string CNPJ { get; set; }
+
+        public EmpresaModel()
+        {
+            this.codigo = codigo;
+            this.nome = nome;
+            this.nomeFantasia = nomeFantasia;
+            this.CNPJ = CNPJ;
+        }
+    }
+}
